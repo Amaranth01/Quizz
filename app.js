aaf05c87422b831922f001080e4321da0b143db7
Amaranth01 <vanessa.nectoux@gmail.com>
2021-12-27 13:39:19 +0100

    step 4

--- app.js
+++ app.js
@@ -25,35 +25,39 @@ const Question = [
         answer: 2
     },
     {
-        Question: "Quelle est la phrase que dit souvent Buzz l'éclair ?",
+        question: "Quelle est la phrase que dit souvent Buzz l'éclair ?",
         choicesAnswer: ["Buzz l'éclair à la rescousse !", "Poussez-vous je suis le roi !", "Pars très loin et ne revient jamais !", "Vers l'infini et au-delàs !"],
         answer: 3
     },
     {
-        Question: "Quels sont les animaux utilisés pour le croquet dans Alice aux pays des marveilles ?",
+        question: "Quels sont les animaux utilisés pour le criquet dans Alice aux pays des marveilles ?",
         choicesAnswer: ["Une mouette et une souris", "Un flamand rose et un hérisson", "Le lièvre et la tortue", "Boule et Bill"],
         answer: 1
     },
     {
-        Question: "Laquelle de ces conditions ne fait pas partie des conditions initiales pour s'occuper d'un mogwaï ?",
+        question: "Laquelle de ces conditions ne fait pas partie des conditions initiales pour s'occuper d'un mogwaï ?",
         choicesAnswer: ["Ne pas les mouiller", "Ne pas les nourrir après minuit", "Ne pas les exposer à la lumière du soleil","Ne pas les toucher"],
         answer: 3
     },
     {
-        Question: "De qui Eivor est-il la réincarnation ?",
+        question: "De qui Eivor est-il la réincarnation ?",
         choicesAnswer: ["De Freya", "D'Odin", "De Loki", "De Thor"],
         answer: 1
     },
     {
-        Question: "Gandalf dans la mythologie Nordique est",
-        choicesAnswer: ["Un Nain","Un elfe","Un humain","Un magicien",],
+        question: "Dans la mythologie nordique Gandalf est",
+        choicesAnswer: ["Un Nain","Un elfe","Un humain","Un magicien"],
         answer: 0
     },
 ]
+
 let index = 0;
-let answers = $('.answers');
+let answers = document.getElementsByClassName('answers');
 let choices = $('#choices');
 let scoreAll = $('#scoreAll');
+let point = 0;
+let score = $('#score');
+
 //hide the elements of questions
 choices.hide();
 scoreAll.hide();
@@ -63,36 +67,33 @@ $('#start').click(function() {
     $('#start').hide();
     choices.show();
     scoreAll.show();
-    seeQuestion();
+    $('.modal').hide();
+    seeQuestion(index);
+    summary();
 });
 
 //See the questions in HTML
-function seeQuestion () {
+function seeQuestion (next) {
     for (let element of Question) {
-    $('#quiz').text(Question[index].question);
-        console.log("sdf")
+    $('#quiz').text(Question[next].question);
+
+        //Create button for the answers
+        for (let i = 0; i < answers.length; i++) {
+            answers[i].innerHTML = (Question[index].choicesAnswer[i]);
+        }
     }
 }
 
-//Create button for the answers
-
-Question[index].choicesAnswer.forEach(function (){
-    answers.text(Question[index].choicesAnswer);
-    console.log("tyu");
-})
-
-answers.click(function (){
+for (let i = 0; i < answers.length; i++) {
+    answers[i].addEventListener('click', function () {
 // Verify if the answer is correct
-    let score = $('#score');
-    let point = 0;
-    if (Question.answer=== Question[index].choicesAnswer.index) {
-        score.text(point++);
-        console.log("aze")
-    }
-    //go to the next question
-    this.Question[index].question++;
-    console.log('test click');
-});
-// créer la fenêtre de récap :D
+        if ( Question[index].answer === i) {
+            score.text(point +=1);
+            console.log(point);
+        }
+        //go to the next question
+        index++;
+        seeQuestion(index);
+    });
+}
 
-console.log(Question)
